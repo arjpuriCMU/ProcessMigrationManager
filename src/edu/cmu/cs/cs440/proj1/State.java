9d656b3d8b365dcb02306bee89a71c1ef4ecbac5
Arjun Puri <arjpuri@andrew.cmu.edu>
2014-09-11 01:05:10 -0400

    after disaster

--- src/edu/cmu/cs/cs440/proj1/State.java
+++
@@ -1,5 +0,0 @@
-package edu.cmu.cs.cs440.proj1;
-
-public enum State {
-	RUNNING, SUSPENDED, DONE, QUIT, START, TOSUSPEND, TEST, TESTDONE
-}
--- src/proj1/NumberOut.java
+++ src/proj1/NumberOut.java
@@ -1,5 +1,28 @@
-package proj1;
+import MigratableProcess;
 
-public class NumberOut {
-
+public class number extends MigratableProcess{
+  
+  private int num = 0;
+  private TransactionalFileOutputStream outstream;
+  
+  public number(String[] args){
+    super(args);
+    outstream = new TransactionalFileOutputStream(args[0]);
+  }
+  
+  public void run(){
+    this.running = true;
+    this.suspended = false;
+    while(!suspended){
+      if (num < 20){
+        outstream.write(num.toString() + "\n");
+      }
+      Thread.sleep(200);
+      
+    }
+  }
+  
+  public void suspend(){
+    
+  }
 }
